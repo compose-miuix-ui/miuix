636157c41727781b6eeea1067cd28ff731342d61
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-03-28 20:10:18 +0800

    Init 28

--- miuix-navigation3-ui/src/skikoMain/kotlin/androidx/navigation3/ui/Utils.skiko.kt
+++ miuix-navigation3-ui/src/skikoMain/kotlin/androidx/navigation3/ui/Utils.skiko.kt
@@ -4,6 +4,11 @@
 package androidx.navigation3.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 actual fun isInMultiWindowMode(): Boolean = false
+
+@Composable
+actual fun getRoundedCorner(): Dp = 0.dp
