3fa5207e63dfb85a85b5dd95752f4a12b18ecd9c
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-08-13 21:08:16 +0800

    library: bump minSdk to 24
    * https://android-review.googlesource.com/c/platform/frameworks/support/+/4197743
    * close #299

--- build-plugins/src/main/kotlin/BuildConfig.kt
+++ build-plugins/src/main/kotlin/BuildConfig.kt
@@ -11,7 +11,7 @@ object BuildConfig {
     const val COMPILE_SDK = 37
     const val COMPILE_SDK_MINOR = 0
     const val TARGET_SDK = 37
-    const val MIN_SDK = 23
+    const val MIN_SDK = 24
     const val BUILD_TOOLS_VERSION = "37.0.0"
     const val JDK_VERSION = 21
 }
