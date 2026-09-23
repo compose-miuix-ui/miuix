ec7699cd1429721ca237d12aa826d7c407a92cc6
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-09-23 14:27:53 +0800

    Update OtherPageSection.kt

--- example/shared/src/commonMain/kotlin/component/OtherPageSection.kt
+++ example/shared/src/commonMain/kotlin/component/OtherPageSection.kt
@@ -23,11 +23,6 @@ fun LazyListScope.otherPageSection() {
                 .padding(horizontal = 12.dp),
         ) {
             ArrowPreference(
-                title = "Glass Components",
-                summary = "Bars, tabs, popup and dialog on glass",
-                onClick = { navigator.push(Route.Glass) },
-            )
-            ArrowPreference(
                 title = "PullToRefresh Test",
                 summary = "Navigate to a PullToRefresh Page",
                 onClick = {
